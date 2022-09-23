7a76e8ff073fc9a6592081a2628f09611da2dbb1
Keisha Hepner <kmhepner90@gmail.com>
2022-09-23 11:10:29 -0700

    BreadPrice(1) test passed

--- Pierres.Tests/ModelTests/PierresTests.cs
+++ Pierres.Tests/ModelTests/PierresTests.cs
@@ -1,4 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using ScrabbleScore.Models;
+using PierresBakery.Models;
 using System;
 using System.Collections.Generic;
+
+namespace Bakery.Test
+{
+  [TestClass]
+  
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadPrice_OneLoafPrice_True()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(5, newBread.BreadPrice(1));
+    }
+  }
+}
