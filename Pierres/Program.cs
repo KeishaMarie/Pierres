34bef168986c3b98163b440ac640210ae9854d47
Keisha Hepner <kmhepner90@gmail.com>
2022-10-02 19:53:41 -0700

    WIP Refreshing memory on Console write and read lines

--- Pierres/Program.cs
+++ Pierres/Program.cs
@@ -5,6 +5,8 @@ class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("Welcome to Pierre's Bakery!");
+    Console.WriteLine("One loaf of bread is $5, two loaves is $10. Or buy multiple and take advantage of our Buy two get one free deal!");
+    Console.WriteLine("Three loaves for $10 or six loaves for $20!")
   }
 }
