4c530f1a63faf8fae5d5ebfc4c170ede8a767a99
Keisha Hepner <kmhepner90@gmail.com>
2022-10-02 18:51:54 -0700

    Update files to reflect respective models and tests

---
+++ Pierres/Models/Bread.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierresBakery.Models
+{
+  public class Bread
+  {
+    public int BreadPrice(int numLoaves)
+    {
+      if (numLoaves == 1)
+      {
+        return 5;
+      }
+      else if (numLoaves == 2)
+      {
+        return 10;
+      }
+      else if (numLoaves >=3)
+      {
+        decimal breadDeal = Decimal.Floor(numLoaves/3);
+        int stringToInt = Convert.ToInt32(breadDeal);
+        return ((numLoaves - stringToInt)*5);
+      }
+      else
+      {
+        return 0;
+      }
+    }
+  }
+
+}
